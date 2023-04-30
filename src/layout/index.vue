889fe853c7f82213122d6350be65cbd86eba1391
jessica <3344538341@qq.com>
2023-04-30 11:20:06 +0800

    搭建基座应用

---
+++ src/layout/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+<template>
+  <el-config-provider namespace="ep">
+    <BaseHeader />
+    <div style="display: flex">
+      <BaseSide />
+      <router-view></router-view>
+    </div>
+  </el-config-provider>
+</template>
+<style scoped></style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,3 @@
+<scritp setup lang="ts"></scritp>
+<template>基座应用首页</template>
+<style scoped></style>
